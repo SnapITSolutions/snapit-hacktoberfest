6f480c65c5f71596b75af1a06da2f9ba71a1fd82
Steve Simmons <steve@snapit.solutions>
2021-07-21 16:12:34 -0500

    Added dummy components

---
+++ src/pages/FAQ.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AccordionList from "../components/AccordionList";
+
+const FAQ = () => (
+  <div className="FAQ" data-test="FAQ-page">
+    <h1>FAQ</h1>
+  </div>
+);
+
+export default FAQ;
