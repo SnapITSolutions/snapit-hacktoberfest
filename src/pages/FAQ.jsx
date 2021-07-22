c2de94dc0416578c2b3d000130fe7d55716eaccb
Steve Simmons <steve@snapit.solutions>
2021-07-22 09:54:29 -0500

    Added real FAQ component

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import AccordionList from "../components/AccordionList";
 
 const FAQ = () => (
   <div className="FAQ" data-test="FAQ-page">
     <h1>FAQ</h1>
+    <AccordionList />
   </div>
 );
 
