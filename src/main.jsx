9ce5a5813e97661d5f1437dc0455c669d9b29411
Steve Simmons <steve@snapit.solutions>
2021-07-28 14:47:21 -0500

    Starting to setup the react-intl for the website

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { IntlProvider } from 'react-intl';
 import App from "./App";
+import Wrapper from "./components/Wrapper";
+
+// const messages = {
+//   first: "This is the first message"
+// }
+
+// console.log("the language is:", language.first);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Wrapper>
+      {/* <IntlProvider locale={userLanguage} key={userLanguage} messages={Spanish}> */}
+      <App />
+      {/* </IntlProvider> */}
+    </Wrapper>
   </React.StrictMode>,
   document.getElementById("root")
 );
