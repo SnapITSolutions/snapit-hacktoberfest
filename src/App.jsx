2df04b2fbb0ad1c2697338b27a46692b7d68455a
Steve Simmons <steve@snapit.solutions>
2021-07-28 14:47:21 -0500

    Starting to setup the react-intl for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
+import { FormattedMessage } from "react-intl";
 import Router from "./router/Router";
+import { Context } from "./components/Wrapper";
 
-const App = () => (
-  <div className="App" data-test="component-app">
-    <Router />
-  </div>
-);
+const App = () => {
+  const context = useContext(Context);
+
+  // const onLanguageChange = (event) => {
+  //   event.preventDefault();
+  //   context.selectLang(event)
+  // }
+
+  return (
+    <div className="App" data-test="component-app">
+      {/* <input onChange={onLanguageChange} value={context.locale}/> */}
+      <select value={context.locale} onChange={context.selectLang}>
+        <option value="en-US">English</option>
+        <option value="es-US">Spanish</option>
+      </select>
+      <FormattedMessage
+        id="first"
+        defaultMessage="this will default to English."
+      />
+      <Router />
+    </div>
+  );
+};
 
 export default App;
