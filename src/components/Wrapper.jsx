e98166248185889da07cacc4cbd3c7b0dc73be76
Steve Simmons <steve@snapit.solutions>
2021-07-29 15:39:14 -0500

    Made site able to display user's preferred language based on their browser settings

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { IntlProvider } from "react-intl";
 import PropTypes from "prop-types";
 import Spanish from "../lang/es-US.json";
@@ -7,39 +7,39 @@ import English from "../lang/en-US.json";
 export const Context = React.createContext();
 
 // detect the language preference of the user's browser
-const userLanguage = window.navigator.userLanguage || window.navigator.language;
-console.log(userLanguage);
+const locale = window.navigator.userLanguage || window.navigator.language;
 
-// let language;
-// if (userLanguage === "en-US") {
-//   language = English;
-// } else {
-//   language = Spanish;
-// }
+let messages;
+if (locale === "en-US") {
+  messages = English;
+} else {
+  messages = Spanish;
+}
 
-const Wrapper = ({ children }) => {
-  const [locale, setLocale] = useState(userLanguage);
-  const [messages, setMessages] = useState(English);
+const Wrapper = ({ children }) => (
+  //     const [locale, setLocale] = useState(userLanguage);
+  //     const [messages, setMessages] = useState(English);
 
-  function selectLang(event) {
-    event.preventDefault();
-    const newLocale = event.target.value;
-    setLocale(newLocale);
-    if (newLocale === "es-US") {
-      setMessages(Spanish);
-    } else {
-      setMessages(English);
-    }
-  }
-
-  return (
-    <Context.Provider value={{ locale, selectLang }}>
-      <IntlProvider locale={locale} messages={messages}>
-        {children}
-      </IntlProvider>
-    </Context.Provider>
-  );
-};
+  //     function selectLang(event) {
+  //         event.preventDefault();
+  //         const newLocale = event.target.value;
+  //         setLocale(newLocale);
+  //         console.log(newLocale)
+  //         console.log(locale);
+  //         if (newLocale === "es-US") {
+  //             setMessages(Spanish);
+  //         } else {
+  //             setMessages(English);
+  //             console.log(messages);
+  //         }
+  //     }
+  //     console.log(locale);
+  <Context.Provider value={{ locale }}>
+    <IntlProvider locale={locale} messages={messages}>
+      {children}
+    </IntlProvider>
+  </Context.Provider>
+);
 
 Wrapper.propTypes = {
   children: PropTypes.element.isRequired,
