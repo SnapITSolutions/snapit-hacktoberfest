2057413843ff98ddd274d511f5e75c761655b742
AndySnapIT <C_Edson.Andrew@cerner.com>
2021-07-30 12:02:17 -0500

    Revert "Merge branch 'main' into docedson-patch-1"
    This reverts commit 5ac1777869becd4f12e47463587f2589516fe39f, reversing
    changes made to 9bfd46b36be02a2881b0448b4217b0f13f1e4e67.

--- src/pages/Registration.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Registration = () => (
-  <div className="Registration" data-test="Registration-page">
-    <h1>Registration</h1>
-  </div>
-);
-
-export default Registration;
