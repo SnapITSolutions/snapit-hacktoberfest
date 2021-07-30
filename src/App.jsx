14f01188927f8b1e323f892f36743f94e178870d
AndySnapIT <C_Edson.Andrew@cerner.com>
2021-07-30 12:02:17 -0500

    Revert "Merge branch 'main' into docedson-patch-1"
    This reverts commit 5ac1777869becd4f12e47463587f2589516fe39f, reversing
    changes made to 9bfd46b36be02a2881b0448b4217b0f13f1e4e67.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import React from "react";
-import Router from "./router/Router";
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = () => (
-  <div className="App" data-test="component-app">
-    <Router />
-  </div>
-);
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button
+            type="button"
+            onClick={() => setCount((currentCount) => currentCount + 1)}
+          >
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
