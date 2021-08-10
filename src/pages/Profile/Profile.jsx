f7d987e3c4eabdfc0041eaeaec2934a1304055b7
Steve Simmons <steve@snapit.solutions>
2021-08-10 15:52:18 -0500

    Added styling changes

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./Profile.css";
 import Avatar from "@material-ui/core/Avatar";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import Box from "@material-ui/core/Box";
 
 const ProfilePage = () => (
-  <div className="container">
+  <Box className="container">
     <div className="profile-pic" data-test="profile-image">
       <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
     </div>
@@ -15,7 +16,7 @@ const ProfilePage = () => (
       <h3>Check your progress</h3>
       <LinearProgress variant="determinate" className="progressBar" />
     </div>
-  </div>
+  </Box>
 );
 
 export default ProfilePage;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Information from "../pages/Information";
-import Profile from "../pages/Profile";
+import Profile from "../pages/Profile/Profile";
 import FAQ from "../pages/FAQ";
 import Registration from "../pages/Registration";
 
