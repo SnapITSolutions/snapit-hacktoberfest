8385fd31ad5c040273c4c2fd7b96695da2026b85
Steve Simmons <steve@snapit.solutions>
2021-08-11 09:04:16 -0500

    Added Home page, and countdown timer component

---
+++ src/components/CountdownTimer.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+const CountdownTimer = () => {
+  const [, setUpdate] = useState(0);
+
+  const eventStart = Date.parse("Sept 30, 2021 24:00:00");
+  const currentTime = new Date();
+  const timeLeft = eventStart - currentTime;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(timeLeft / (1000 * 60 * 60));
+  const mins = Math.floor(timeLeft / (1000 * 60));
+  const secs = Math.floor(timeLeft / 1000);
+
+  const d = days;
+  const h = hours - days * 24;
+  const m = mins - hours * 60;
+  const s = secs - mins * 60;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdate((seconds) => seconds - 1);
+      // console.log('This will run every second!');
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="CountDownTimer">
+      <div id="displayTime">
+        <ul id="labels">
+          <li>DAYS</li>
+          <li>HOURS</li>
+          <li>MINUTES</li>
+          <li>SECONDS</li>
+        </ul>
+        <ul id="numbers">
+          <li className="Display">{d} </li>
+          <li className="Display">{h} </li>
+          <li className="Display">{m} </li>
+          <li className="Display">{s} </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navigation from "../components/Navigation";
 import Information from "../pages/Information";
+import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import FAQ from "../pages/FAQ";
 import PrivateRoute from "./PrivateRoute";
@@ -13,7 +14,8 @@ const Router = () => (
     <BrowserRouter basename="/HacktoberFest">
       <Navigation />
       <Switch>
-        <Route path="/" exact component={Information} />
+        <Route path="/" exact component={Home} />
+        <Route path="/information" component={Information} />
         <Route path="/faq" component={FAQ} />
         <Route path="/login" component={Login} />
         <PrivateRoute path="/profile" component={Profile} />
