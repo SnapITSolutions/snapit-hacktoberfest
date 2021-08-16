18b5d6aeb7864123377147d94dfeee01ef9e04a1
Steve Simmons <steve@snapit.solutions>
2021-08-16 11:56:32 -0500

    Refactored profile page using strict MUI

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,97 +1,79 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
 
-const styles = {
-  card: {
-    minWidth: 700,
-    margin: "70px 0 0 0",
-    boxShadow: "4px 4px 5px grey",
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(8),
+    height: theme.spacing(8),
   },
-  box: {
-    height: "70px",
-    // backgroundColor: "green",
-  },
-  avatar: {
-    width: "80px",
-    height: "80px",
-    position: "absolute",
-    marginLeft: "60px",
-    marginTop: " 10px",
-  },
-  userName: {
-    textAlign: "left",
-    fontSize: 26,
-    marginLeft: "150px",
-    paddingTop: "30px",
-  },
-  userInfo: {
-    width: "60%",
-    paddingTop: "30px",
-    margin: "auto",
-    textAlign: "left",
-  },
-  button: {
-    margin: "30px auto 10px",
-    backgroundColor: "lightblue",
-    padding: "0 20px",
-  },
-};
+}));
 
-function Profile(props) {
-  const { classes } = props;
+function Profile() {
+  const classes = useStyles();
 
   return (
-    <Card className={classes.card} data-test="Profile-page">
-      <CardContent>
-        <Box className={classes.box}>
-          <Avatar
-            alt="Remy Sharp"
-            src="/static/images/avatar/1.jpg"
-            className={classes.avatar}
-            data-test="profile-avatar"
-          />
-          <Typography
-            className={classes.userName}
-            variant="h5"
-            component="h2"
-            data-test="user-name"
-          >
-            User Name
-          </Typography>
-        </Box>
-        <Typography
-          className={classes.userInfo}
-          component="p"
-          data-test="user-info"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </Typography>
-        <CardActions>
-          <Button
-            size="small"
-            className={classes.button}
-            data-test="profile-button"
-          >
-            Link to Users Github Account
-          </Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+    <Box mt={16} mx="auto" width="70%" height={12}>
+      <Paper>
+        <Grid container spacing={8}>
+          <Box mx={12} mb={6} mt={5}>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Grid container direction="row" alignItems="center">
+                    <Grid item>
+                      <Avatar
+                        className={classes.large}
+                        alt="User's Name"
+                        src="/static/images/grid/complex.jpg"
+                        width="80%"
+                        height="330"
+                      />
+                    </Grid>
+                    <Grid item>
+                      <Box ml="15px">
+                        <Typography gutterBottom variant="h5">
+                          Users Name
+                        </Typography>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                  <Box mt="25px" mb="21px">
+                    <Typography variant="body2" gutterBottom align="left">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </Typography>
+                  </Box>
+                </Grid>
+                <Grid item>
+                  <ButtonBase className={classes.image}>
+                    <Button
+                      backgroundColor="primary"
+                      href="#"
+                      size="small"
+                      variant="contained"
+                      color="primary"
+                      data-test="profile-button"
+                    >
+                      Link to Users Github Account
+                    </Button>
+                  </ButtonBase>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+      </Paper>
+    </Box>
   );
 }
 
-Profile.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Profile);
+export default Profile;
