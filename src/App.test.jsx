29a04258baf37987d292391bb609b9d1c07e47dc
as082003 <c_shivers_aaron@cerner.com>
2021-08-18 14:18:02 -0500

    added deleted files back into project

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import FAQ from "./pages/FAQ";
+import Information from "./pages/Information";
+import Profile from "./pages/Profile";
+import Registration from "./pages/Registration";
+import AccordionList from "./components/AccordionList";
 
-describe("App", () => {
-  const setup = () => shallow(<App />);
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test="${val}"]`);
 
-  test("renders without error", () => {
-    const wrapper = setup();
-    expect(wrapper.exists()).toBeTruthy();
-  });
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders FAQ page", () => {
+  const wrapper = shallow(<FAQ />);
+  const faqComponent = findByTestAttr(wrapper, "FAQ-page");
+  expect(faqComponent.length).toBe(1);
+});
+
+test("renders Information page", () => {
+  const wrapper = shallow(<Information />);
+  const infoComponent = findByTestAttr(wrapper, "information-display");
+  expect(infoComponent.length).toBe(1);
+});
+
+test("renders Profile page", () => {
+  const wrapper = shallow(<Profile />);
+  const profileComponent = findByTestAttr(wrapper, "Profile-page");
+  expect(profileComponent.length).toBe(1);
+});
+
+test("renders Registration page", () => {
+  const wrapper = shallow(<Registration />);
+  const registrationComponent = findByTestAttr(wrapper, "Registration-page");
+  expect(registrationComponent.length).toBe(1);
+});
+
+test("renders Accordion List", () => {
+  const wrapper = shallow(<AccordionList />);
+  const accordionListComponent = findByTestAttr(
+    wrapper,
+    "component-accordionList"
+  );
+  expect(accordionListComponent.length).toBe(1);
 });
---
+++ src/pages/Registration.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Registration = () => (
+  <div className="Registration" data-test="Registration-page">
+    <h1>Registration</h1>
+  </div>
+);
+
+export default Registration;
