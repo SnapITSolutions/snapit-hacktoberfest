9e9d5b7adbccb2071f1bbafeeb65fa03e90e9c6e
Steve Simmons <steve@snapit.solutions>
2021-08-19 16:27:04 -0500

    Added Intl to paragraphs

--- src/pages/Information/Information.jsx
+++ src/pages/Information/Information.jsx
@@ -11,13 +11,10 @@ const Information = () => (
         />
       </h2>
       <p data-test="para-display">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        <FormattedMessage
+          id="Info.first-paragraph"
+          defaultMessage="Lorem ipsum..."
+        />
       </p>
       <img
         data-test="image-display"
@@ -34,8 +31,10 @@ const Information = () => (
         />
       </h2>
       <p data-test="para-display">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
+        <FormattedMessage
+          id="Info.second-paragraph"
+          defaultMessage="Lorem ipsum..."
+        />
       </p>
     </div>
     <div>
@@ -46,8 +45,10 @@ const Information = () => (
         />
       </h2>
       <p data-test="para-display">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.{" "}
+        <FormattedMessage
+          id="Info.third-paragraph"
+          defaultMessage="Lorem ipsum..."
+        />
       </p>
     </div>
   </>
