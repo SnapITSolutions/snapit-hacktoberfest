d97899c4275f823cd739d2c907485807b6e3800c
Steve Simmons <steve@snapit.solutions>
2021-08-20 09:27:50 -0500

    Made a new dir for Countdown Timer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import CountdownTimer from "../../components/CountdownTimer";
-import "./Home.css";
+import CountdownTimer from "../../components/CountdownTimer/CountdownTimer";
+import "../../components/CountdownTimer/CountdownTimer.css";
 
 const Home = () => (
   <div className="Home" data-test="Home-page">
