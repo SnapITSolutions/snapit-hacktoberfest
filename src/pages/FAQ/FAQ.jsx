6132e45ae0ee4991c1b728723fde67f577afd950
Steve Simmons <steve@snapit.solutions>
2021-08-24 14:57:52 -0500

    Deleted App.css and added inline styling to Navigation and FAQ files

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -3,7 +3,7 @@ import Box from "@material-ui/core/Box";
 import AccordionList from "../../components/AccordionList";
 
 const FAQ = () => (
-  <Box mt="40" mx="auto ">
+  <Box mt="40" mx="auto " style={{ backgroundColor: "aliceblue" }}>
     <div className="FAQ" data-test="FAQ-page">
       <h1 data-test="heading-display">FAQ</h1>
       <AccordionList data-test="FAQ-list" />
