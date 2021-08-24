d007b8523c786b50099b43c6bab0bd7d613c9e18
Steve Simmons <steve@snapit.solutions>
2021-08-24 16:14:48 -0500

    Moved IntlProvider and locale to Wrapper component

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { IntlProvider } from "react-intl";
-import App from "./App";
 import Wrapper from "./components/Wrapper";
-import Spanish from "./lang/es-US.json";
-import English from "./lang/en-US.json";
-
-const locale = window.navigator.userLanguage || window.navigator.language;
-
-let messages;
-if (locale === "en-US") {
-  messages = English;
-} else {
-  messages = Spanish;
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Wrapper>
-      <IntlProvider locale={locale} messages={messages}>
-        <App />
-      </IntlProvider>
-    </Wrapper>
+    <Wrapper />
   </React.StrictMode>,
   document.getElementById("root")
 );
