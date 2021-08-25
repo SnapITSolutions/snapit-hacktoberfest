40bb5b6e0a3804e918f18f3cf393cc3613c05c23
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2021-08-25 09:08:45 -0500

    Moved AuthButton to components directory

--- src/components/AuthButton.jsx
+++ src/components/AuthButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import { useAuthContext } from "../../context/AuthContext";
+import { useAuthContext } from "../context/AuthContext";
 
 const AuthButton = () => {
   const history = useHistory();
