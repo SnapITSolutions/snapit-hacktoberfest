4160af1840a173306e06d75e0f07cbf95f669116
Steve Simmons <steve@snapit.solutions>
2021-08-26 15:19:29 -0500

    "Refactored code in test files"

--- src/pages/FAQ/FAQ.test.jsx
+++ src/pages/FAQ/FAQ.test.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import { shallow } from "enzyme";
 import FAQ from "./FAQ";
 
-const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+describe("FAQ", () => {
+  const setup = () => shallow(<FAQ />);
 
-test("renders FAQ page", () => {
-  const wrapper = shallow(<FAQ />);
-  const FAQComponent = findByTestAttr(wrapper, "FAQ-page");
-  expect(FAQComponent.length).toBe(1);
-});
+  const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
-test("renders FAQ Accordion list", () => {
-  const wrapper = shallow(<FAQ />);
-  const countdownDisplay = findByTestAttr(wrapper, "FAQ-list");
-  expect(countdownDisplay.length).toBe(1);
-});
+  test("renders FAQ page", () => {
+    const wrapper = setup();
+    const FAQComponent = findByTestAttr(wrapper, "FAQ-page");
+    expect(FAQComponent.length).toBe(1);
+  });
 
-test("renders FAQ heading", () => {
-  const wrapper = shallow(<FAQ />);
-  const countdownDisplay = findByTestAttr(wrapper, "heading-display");
-  expect(countdownDisplay.length).toBe(1);
+  test("renders FAQ Accordion list", () => {
+    const wrapper = setup();
+    const countdownDisplay = findByTestAttr(wrapper, "FAQ-list");
+    expect(countdownDisplay.length).toBe(1);
+  });
+
+  test("renders FAQ heading", () => {
+    const wrapper = setup();
+    const countdownDisplay = findByTestAttr(wrapper, "heading-display");
+    expect(countdownDisplay.length).toBe(1);
+  });
 });
--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import { shallow } from "enzyme";
 import Home from "./Home";
 
-const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+describe("Home", () => {
+  const setup = () => shallow(<Home />);
 
-test("renders Home page", () => {
-  const wrapper = shallow(<Home />);
-  const homeComponent = findByTestAttr(wrapper, "Home-page");
-  expect(homeComponent.length).toBe(1);
-});
+  const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
-test("renders CountdownTimer", () => {
-  const wrapper = shallow(<Home />);
-  const countdownDisplay = findByTestAttr(wrapper, "countdown-display");
-  expect(countdownDisplay.length).toBe(1);
-});
+  test("renders Home page", () => {
+    const wrapper = setup();
+    const homeComponent = findByTestAttr(wrapper, "Home-page");
+    expect(homeComponent.length).toBe(1);
+  });
 
-test("renders CountdownTimer", () => {
-  const wrapper = shallow(<Home />);
-  const countdownDisplay = findByTestAttr(wrapper, "heading-display");
-  expect(countdownDisplay.length).toBe(1);
+  test("renders CountdownTimer", () => {
+    const wrapper = setup();
+    const countdownDisplay = findByTestAttr(wrapper, "countdown-display");
+    expect(countdownDisplay.length).toBe(1);
+  });
+
+  test("renders CountdownTimer", () => {
+    const wrapper = setup();
+    const countdownDisplay = findByTestAttr(wrapper, "heading-display");
+    expect(countdownDisplay.length).toBe(1);
+  });
 });
