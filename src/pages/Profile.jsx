2257c4bd8c8b5b22056ecad720a844ffed1b79bc
Steve Simmons <steve@snapit.solutions>
2021-08-27 11:08:57 -0500

    Revert profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
 
 const Profile = () => (
-  <Box mt="40" mx="auto ">
-    <div className="Profile" data-test="Profile-page">
-      <h1>Profile</h1>
-    </div>
-  </Box>
+  <div className="Profile" data-test="profile-page">
+    <h1>Profile</h1>
+    <h2>Manage Account</h2>
+  </div>
 );
 
 export default Profile;
