31a702bc390f1e8a46e07c0b3d48a3fd10d7913f
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2021-09-02 13:23:37 -0500

    Created 404 page and route

---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PageNotFound = () => (
+  <>
+    <div data-test="page-not-found-display">
+      <h1 data-test="heading-display">404</h1>
+      <h2 data-test="heading-display">Page not found</h2>
+      <img data-test="image-display" src="" alt="error" />
+      <p data-test="para-display">
+        We are sorry, the page you are looking for could not be found.
+      </p>
+    </div>
+  </>
+);
+
+export default PageNotFound;
---
+++ src/pages/PageNotFound/PageNotFound.test.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { shallow } from "enzyme";
+import PageNotFound from "./PageNotFound";
+
+describe("notFound", () => {
+  const setup = () => shallow(<PageNotFound />);
+
+  const findByTestAttribute = (wrapper, val) =>
+    wrapper.find(`[data-test='${val}']`);
+
+  test("renders without error", () => {
+    const wrapper = setup();
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  test("renders page not found display", () => {
+    const wrapper = setup();
+    const pageNotFoundDisplay = findByTestAttribute(
+      wrapper,
+      "page-not-found-display"
+    );
+    expect(pageNotFoundDisplay.length).toBe(1);
+  });
+
+  test("renders first heading display", () => {
+    const wrapper = setup();
+    const headingDisplay = findByTestAttribute(wrapper, "heading-display");
+    expect(headingDisplay.length).toBe(2);
+  });
+
+  test("renders paragraph display", () => {
+    const wrapper = setup();
+    const paraDisplay = findByTestAttribute(wrapper, "para-display");
+    expect(paraDisplay.length).toBe(1);
+  });
+
+  test("renders image display", () => {
+    const wrapper = setup();
+    const imageDisplay = findByTestAttribute(wrapper, "image-display");
+    expect(imageDisplay.length).toBe(1);
+  });
+});
