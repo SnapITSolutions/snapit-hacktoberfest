a9a0378785ee7afb96268c1ca797c63ca3c0a6cd
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2021-09-03 12:18:07 -0500

    Moved signout to AvatarMenu

--- src/components/AuthButton/AuthButton.jsx
+++ src/components/AuthButton/AuthButton.jsx
@@ -1,26 +1,11 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import Button from "@mui/material/Button";
 import { useAuthContext } from "../../contexts/AuthContext";
 
 const AuthButton = () => {
-  const history = useHistory();
-  const { user, signout } = useAuthContext();
+  const { user } = useAuthContext();
 
   return user.isAuthenticated ? (
-    <p>
-      Welcome!{" "}
-      <Button
-        size="large"
-        variant="contained"
-        color="secondary"
-        onClick={() => {
-          signout(() => history.push("/"));
-        }}
-      >
-        Sign out
-      </Button>
-    </p>
+    <p>Welcome! </p>
   ) : (
     <p>You are not logged in.</p>
   );
