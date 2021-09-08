5c76bef3be9fa72058bc0dc043a421cc141aa408
Steve Simmons <steve@snapit.solutions>
2021-09-08 10:25:05 -0500

    Refactored code and deleted IntlContext.jsx file

--- src/Intl.test.jsx
+++ src/Intl.test.jsx
@@ -1,12 +1,28 @@
-import { locale } from "./context/IntlContext";
+import React from "react";
+// import { formatMessage } from "@formatjs/intl";
+import { FormattedMessage } from "react-intl";
+import useLocale from "./hooks/useLocale";
 
-jest.mock("react-intl", () => ({
-  ...jest.requireActual("react-intl"),
-  useIntl: () => ({
-    formatMessage: jest.fn(),
-  }),
-}));
+// jest.mock("react-intl", () => ({
+//   ...jest.requireActual("react-intl"),
+//   useIntl: () => ({
+//     formatMessage: jest.fn(),
+//   }),
+// }));
 
 test("the locale is either Spanish or English", () => {
+  const { locale } = useLocale();
   expect(locale).toBe("en-US" || "es-US");
+});
+
+test("the message should say Home", () => {
+  // const {locale} = useLocale();
+  const message = <FormattedMessage id="Home-nav" />;
+  expect(message).toBe("Home");
+});
+
+test("the message should say Casa", () => {
+  // const {locale} = useLocale();
+  const message = <FormattedMessage id="Home-nav" />;
+  expect(message).toBe("Casa");
 });
--- src/hooks/useLocale.jsx
+++ src/hooks/useLocale.jsx
@@ -1,18 +1,22 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import Spanish from "../lang/es-US.json";
 import English from "../lang/en-US.json";
 
 // detect the language preference of the user's browser
 const useLocale = () => {
   const locale = window.navigator.userLanguage || window.navigator.language;
-  const [messages, setMessages] = useState(English);
-  useEffect(() => {
-    if (locale === "en-US") {
-      setMessages(English);
-    } else {
-      setMessages(Spanish);
-    }
-  }, [locale]);
+  // const [messages, setMessages] = useState(English);
+
+  // useEffect(() => {
+  let messages;
+  if (locale === "en-US") {
+    messages = English;
+    // setMessages(English);
+  } else {
+    messages = Spanish;
+    // setMessages(Spanish);
+  }
+  // }, [locale]);
   return { locale, messages };
 };
 
