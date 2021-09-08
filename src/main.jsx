991a9712e849420f38a6f066a22257ee4671ebb5
Steve Simmons <steve@snapit.solutions>
2021-09-08 10:25:05 -0500

    Refactored code and deleted IntlContext.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { IntlContextProvider } from "./context/IntlContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <IntlContextProvider>
-      <App />
-    </IntlContextProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
