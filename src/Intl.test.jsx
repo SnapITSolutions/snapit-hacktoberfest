ebe40e9091ba6fe1417e754e9a47c5fd8af824cd
Steve Simmons <steve@snapit.solutions>
2021-09-10 13:58:33 -0500

    Delete Intl.test.jsx

--- src/Intl.test.jsx
+++
@@ -1,6 +0,0 @@
-import useLocale from "./hooks/useLocale";
-
-test("the locale is either Spanish or English", () => {
-  const { locale } = useLocale();
-  expect(locale).toBe("en-US" || "es-US");
-});
