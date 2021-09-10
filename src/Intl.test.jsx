eb3a8725936a6436941f28233e464c71528802de
Steve Simmons <steve@snapit.solutions>
2021-09-10 13:49:44 -0500

    Remove commented out code

--- src/Intl.test.jsx
+++ src/Intl.test.jsx
@@ -1,20 +1,6 @@
-// import React from "react";
-// import { FormattedMessage } from "react-intl";
 import useLocale from "./hooks/useLocale";
 
 test("the locale is either Spanish or English", () => {
   const { locale } = useLocale();
   expect(locale).toBe("en-US" || "es-US");
 });
-
-// test("the message should say Home", () => {
-//   // const {locale} = useLocale();
-//   const message = <FormattedMessage id="Home-nav" />;
-//   expect(message).toBe("Home");
-// });
-
-// test("the message should say Casa", () => {
-//   // const {locale} = useLocale();
-//   const message = <FormattedMessage id="Home-nav" />;
-//   expect(message).toBe("Casa");
-// });
--- src/hooks/useLocale.jsx
+++ src/hooks/useLocale.jsx
@@ -1,22 +1,16 @@
-// import { useState, useEffect } from "react";
 import Spanish from "../lang/es-US.json";
 import English from "../lang/en-US.json";
 
 // detect the language preference of the user's browser
 const useLocale = () => {
   const locale = window.navigator.userLanguage || window.navigator.language;
-  // const [messages, setMessages] = useState(English);
 
-  // useEffect(() => {
   let messages;
   if (locale === "en-US") {
     messages = English;
-    // setMessages(English);
   } else {
     messages = Spanish;
-    // setMessages(Spanish);
   }
-  // }, [locale]);
   return { locale, messages };
 };
 
