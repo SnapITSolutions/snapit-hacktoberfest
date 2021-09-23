d65b83e7620b75a3666991546e6fbee2675f793d
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2021-09-23 12:48:49 -0500

    Create AccordionList directory, move files and update import paths

--- src/components/AccordionList/AccordionList.test.jsx
+++ src/components/AccordionList/AccordionList.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { mountWithIntl } from "../utils/intl-enzyme-test-helper";
+import { mountWithIntl } from "../../utils/intl-enzyme-test-helper";
 import AccordionList from "./AccordionList";
 
 describe("FAQ AccordionList", () => {
