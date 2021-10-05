90cf40287198fd3f5d8988001bbc9a33ea2f37c0
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2021-10-05 11:19:27 -0500

    Fix: Extract Accordion Data into an Array (#59)
    * Extract Accordion data into array, fix test, update intl files
    
    * move array to new file
    
    * update text in defaultMessage and intl json files
    
    * Change AccordionListData to js file

---
+++ src/components/AccordionList.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { FormattedMessage } from "react-intl";
+import AccordionListData from "./AccordionList/AccordionListData";
+
+const AccordionList = () => {
+  const displayAccordionList = () => (
+    <div data-test="component-accordionList">
+      {AccordionListData.map(({ summary, details }) => (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography data-test="AccordionList-question">
+              <FormattedMessage
+                id={summary.id}
+                defaultMessage={summary.value}
+              />
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <FormattedMessage
+                id={details.id}
+                defaultMessage={details.value}
+              />
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  );
+  return displayAccordionList();
+};
+
+export default AccordionList;
---
+++ src/components/AccordionList/AccordionListData.js
@@ -0,0 +1,88 @@
+const AccordionListData = [
+  {
+    summary: {
+      id: "FAQ-list-header1",
+      value: {
+        defaultMessage: "Question 1",
+      },
+    },
+    details: {
+      id: "FAQ-list-answer1",
+      value: {
+        defaultMessage: "Answer 1",
+      },
+    },
+  },
+  {
+    summary: {
+      id: "FAQ-list-header2",
+      value: {
+        defaultMessage: "Question 2",
+      },
+    },
+    details: {
+      id: "FAQ-list-answer2",
+      value: {
+        defaultMessage: "Answer 2",
+      },
+    },
+  },
+  {
+    summary: {
+      id: "FAQ-list-header3",
+      value: {
+        defaultMessage: "Question 3",
+      },
+    },
+    details: {
+      id: "FAQ-list-answer3",
+      value: {
+        defaultMessage: "Answer 3",
+      },
+    },
+  },
+  {
+    summary: {
+      id: "FAQ-list-header4",
+      value: {
+        defaultMessage: "Question 4",
+      },
+    },
+    details: {
+      id: "FAQ-list-answer4",
+      value: {
+        defaultMessage: "Answer 4",
+      },
+    },
+  },
+  {
+    summary: {
+      id: "FAQ-list-header5",
+      value: {
+        defaultMessage: "Question 5",
+      },
+    },
+    details: {
+      id: "FAQ-list-answer5",
+      value: {
+        defaultMessage: "Answer 5",
+      },
+    },
+  },
+  {
+    summary: {
+      id: "FAQ-list-header6",
+      value: {
+        defaultMessage: "Question 6",
+      },
+    },
+    details: {
+      id: "FAQ-list-answer6",
+      value: {
+        defaultMessage: "Answer 6",
+      },
+    },
+  },
+];
+
+export default AccordionListData;
