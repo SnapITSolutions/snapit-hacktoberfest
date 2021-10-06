ee24bb572ba3b2cd2af4237956ad9b62932577f5
Steve Simmons <steve@snapit.solutions>
2021-10-06 11:54:59 -0500

    remove unnecessary function

--- src/pages/Information/Information.jsx
+++ src/pages/Information/Information.jsx
@@ -4,51 +4,48 @@ import { FormattedMessage } from "react-intl";
 import informationData from "./informationData";
 
 const Information = () => {
-  const displayInfo = () => {
-    let i = 0;
-    return (
-      <>
-        <div data-test="information-display">
-          {informationData.map(({ header, paragraph }) => {
-            const result = (
-              <div key={header.id}>
-                <Typography
-                  m={4}
-                  variant="h4"
-                  data-test="heading-display"
-                  textAlign="center"
-                >
+  let i = 0;
+  return (
+    <>
+      <div data-test="information-display">
+        {informationData.map(({ header, paragraph }) => {
+          const result = (
+            <div key={header.id}>
+              <Typography
+                m={4}
+                variant="h4"
+                data-test="heading-display"
+                textAlign="center"
+              >
+                <FormattedMessage
+                  id={header.id}
+                  defaultMessage={header.values}
+                />
+                <Typography variant="body1" pt={1} data-test="para-display">
                   <FormattedMessage
-                    id={header.id}
-                    defaultMessage={header.values}
+                    id={paragraph.id}
+                    defaultMessage={paragraph.values}
                   />
-                  <Typography variant="body1" pt={1} data-test="para-display">
-                    <FormattedMessage
-                      id={paragraph.id}
-                      defaultMessage={paragraph.values}
-                    />
-                  </Typography>
                 </Typography>
-                {i === 0 && (
-                  <Box justifyContent="center" display="flex">
-                    <img
-                      data-test="image-display"
-                      src="../src/assets/pumpkin_PNG86719.png"
-                      alt="hacktoberfest"
-                      style={{ height: 100 }}
-                    />
-                  </Box>
-                )}
-              </div>
-            );
-            i += 1;
-            return result;
-          })}
-        </div>
-      </>
-    );
-  };
-  return displayInfo();
+              </Typography>
+              {i === 0 && (
+                <Box justifyContent="center" display="flex">
+                  <img
+                    data-test="image-display"
+                    src="../src/assets/pumpkin_PNG86719.png"
+                    alt="hacktoberfest"
+                    style={{ height: 100 }}
+                  />
+                </Box>
+              )}
+            </div>
+          );
+          i += 1;
+          return result;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Information;
