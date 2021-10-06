c4a5019227ad19969123b7f747f45d393b73dc11
hvgithub <hvgithub@users.noreply.github.com>
2021-10-06 19:25:49 -0400

    Implemented code review feedback

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,8 +3,20 @@ import { FormattedMessage } from "react-intl";
 
 const NotFound = () => (
   <div data-test="page-not-found-display">
-    <h1 data-test="heading-display">404</h1>
-    <h2 data-test="heading-display">Page not found</h2>
+    <h1 data-test="heading-display">
+       <FormattedMessage
+              id="notFound-header-h1"
+              description="Not Found HTTP Error Code 404"
+              defaultMessage="404"
+        />  
+    </h1>
+    <h2 data-test="heading-display">
+        <FormattedMessage
+              id="notFound-header-h2"
+              description="Not Found HTTP Page Not Found"
+              defaultMessage="Page not found"
+        />  
+    </h2>
     <img data-test="image-display" src="" alt="error" />
     <p data-test="para-display">
           <FormattedMessage
