906e92d02bc614ec589eb0985fdf1ab0f325e762
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2021-10-08 15:23:58 -0500

    Update table and styling

--- src/pages/Contributor/ContributorTable.jsx
+++ src/pages/Contributor/ContributorTable.jsx
@@ -15,24 +15,44 @@ const rows = [
   createData("Cami Taylor", "github.com/camitaylor"),
   createData("Steve Simmons", "github.com/stevesimmons"),
   createData("Aaron Shivers", "github.com/aaronshivers"),
-  createData("Becca Simmons", "github.com/beccasimmions"),
+  createData("Becca Simmons", "github.com/beccasimmons"),
   createData("Lis Phelps", "github.com/lisphelps"),
 ];
 
 const ContributorTable = () => (
-  <TableContainer component={Paper} sx={{ width: 500 }}>
+  <TableContainer component={Paper} sx={{ width: 700 }}>
     <Table
       sx={{
-        width: 600,
+        width: 700,
         backgroundColor: "primary.main",
-        color: "text.primary",
       }}
       aria-label="simple table"
     >
       <TableHead>
         <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell align="left">Github</TableCell>
+          <TableCell
+            sx={{
+              color: "text.secondary",
+              fontSize: 20,
+              fontWeight: "bold",
+              pl: 10,
+              backgroundColor: "secondary.dimgray",
+            }}
+          >
+            Name
+          </TableCell>
+          <TableCell
+            align="left"
+            sx={{
+              color: "text.secondary",
+              fontSize: 20,
+              fontWeight: "bold",
+              pl: 15,
+              backgroundColor: "secondary.dimgray",
+            }}
+          >
+            Github
+          </TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
@@ -41,11 +61,16 @@ const ContributorTable = () => (
             key={row.name}
             sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
           >
-            <TableCell component="th" scope="row">
+            <TableCell
+              component="th"
+              scope="row"
+              sx={{ color: "text.secondary", pl: 10 }}
+            >
               {row.name}
             </TableCell>
-            {/* <TableCell align="right">{row.name}</TableCell> */}
-            <TableCell align="left">{row.github}</TableCell>
+            <TableCell align="left" sx={{ color: "text.secondary", pl: 15 }}>
+              {row.github}
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
