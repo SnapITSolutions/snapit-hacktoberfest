9ef916a1cd043fe6ab7512a23de5cca5f1004d52
Tom Chestnut <tchestnut85@gmail.com>
2021-10-08 22:51:00 -0400

    Updated Home to render different heading and a link to HacktoberFest site during HacktoberFest

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,59 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
-import { Grid } from "@mui/material";
+import { Grid, Link } from "@mui/material";
 import CountdownTimer from "../../components/CountdownTimer";
+import "./Home.css";
 import "../../components/CountdownTimer/CountdownTimer.css";
+import { HACKTOBERFEST_URL } from "../../utils/constants";
 
-const Home = () => (
-  <div className="Home" data-test="Home-page">
-    <Grid container justifyContent="center">
-      <h1 data-test="heading-display">
-        <FormattedMessage
-          id="Home-header"
-          defaultMessage="SnapIT Hacktoberfest Starts In:"
-        />
-      </h1>
-      <CountdownTimer data-test="countdown-display" />
-    </Grid>
-  </div>
-);
+const Home = () => {
+  const present = new Date();
+  const octoberStart = new Date("Oct 01 2021");
+  const novemberStart = new Date("Nov 01 2021");
+  const isHacktoberfest = present >= octoberStart && present < novemberStart;
+
+  return (
+    <div className="Home" data-test="Home-page">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+      >
+        {!isHacktoberfest ? (
+          <>
+            <h1 data-test="heading-display">
+              <FormattedMessage
+                id="Home-header"
+                defaultMessage="SnapIT Hacktoberfest Starts In:"
+              />
+            </h1>
+            <CountdownTimer data-test="countdown-display" />
+          </>
+        ) : (
+          <>
+            <h1>
+              <FormattedMessage
+                id="Home-begun"
+                defaultMessage="HacktoberFest is here!"
+              />
+            </h1>
+            <Link
+              href={HACKTOBERFEST_URL}
+              className="link"
+              target="_blank"
+              rel="noopener"
+            >
+              <FormattedMessage
+                id="Home-link"
+                defaultMessage="Sign up to start hacking!"
+              />
+            </Link>
+          </>
+        )}
+      </Grid>
+    </div>
+  );
+};
 
 export default Home;
