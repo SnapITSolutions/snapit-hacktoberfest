6f0b40a8219bfb8966281369182b318f2c3e4068
Steve Simmons <steve@snapit.solutions>
2021-10-09 13:19:06 -0500

    refactor profile page function

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,52 +5,50 @@ import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 
-function Profile() {
-  return (
-    <Box mt={16} mx="auto" width="70%" height={12} data-test="Profile-page">
-      <Paper>
-        <Grid container spacing={8}>
-          <Box mx={12} mb={6} mt={5}>
-            <Grid item xs={12} sm container>
-              <Grid item xs container direction="column" spacing={2}>
-                <Grid item xs>
-                  <Grid container direction="row" alignItems="center">
-                    <Grid item>
-                      <Avatar
-                        sx={{ width: 60, height: 60 }}
-                        alt="User's Name"
-                        src="/static/images/grid/complex.jpg"
-                        data-test="profile-avatar"
-                      />
-                    </Grid>
-                    <Grid item>
-                      <Box ml="15px">
-                        <Typography
-                          gutterBottom
-                          variant="h5"
-                          data-test="user-name"
-                        >
-                          Users Name
-                        </Typography>
-                      </Box>
-                    </Grid>
+const Profile = () => (
+  <Box mt={16} mx="auto" width="70%" height={12} data-test="Profile-page">
+    <Paper>
+      <Grid container spacing={8}>
+        <Box mx={12} mb={6} mt={5}>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Grid container direction="row" alignItems="center">
+                  <Grid item>
+                    <Avatar
+                      sx={{ width: 60, height: 60 }}
+                      alt="User's Name"
+                      src="/static/images/grid/complex.jpg"
+                      data-test="profile-avatar"
+                    />
                   </Grid>
-                  <Box mt="25px" mb="21px" data-test="user-info">
-                    <Typography variant="body2" gutterBottom align="left">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </Typography>
-                  </Box>
+                  <Grid item>
+                    <Box ml="15px">
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        data-test="user-name"
+                      >
+                        Users Name
+                      </Typography>
+                    </Box>
+                  </Grid>
                 </Grid>
+                <Box mt="25px" mb="21px" data-test="user-info">
+                  <Typography variant="body2" gutterBottom align="left">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </Typography>
+                </Box>
               </Grid>
             </Grid>
-          </Box>
-        </Grid>
-      </Paper>
-    </Box>
-  );
-}
+          </Grid>
+        </Box>
+      </Grid>
+    </Paper>
+  </Box>
+);
 
 export default Profile;
