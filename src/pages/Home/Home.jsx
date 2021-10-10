4616c824298ea397293e8f6791731046151a3401
Tom Chestnut <tchestnut85@gmail.com>
2021-10-10 05:54:27 -0400

    Fix: Welcome Page (After Countdown) (#97)
    * Updated Home to render different heading and a link to HacktoberFest site during HacktoberFest
    
    * Made a util file for hacktoberfestCheck and used in Home
    
    * fixed error in Home.jsx
    
    * Added tests to Home.test.jsx for the HacktoberFest link. Updated some tests to run depending on if HacktoberFest is active.
    
    Co-authored-by: Aaron <42848750+aaronshivers@users.noreply.github.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,52 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
-import { Grid } from "@mui/material";
+import { Grid, Link } from "@mui/material";
 import CountdownTimer from "../../components/CountdownTimer";
+import "./Home.css";
 import "../../components/CountdownTimer/CountdownTimer.css";
+import { HACKTOBERFEST_URL } from "../../utils/constants";
+import isHacktoberfest from "../../utils/hacktoberfestCheck";
 
 const Home = () => (
   <div className="Home" data-test="Home-page">
-    <Grid container justifyContent="center">
-      <h1 data-test="heading-display">
-        <FormattedMessage
-          id="Home-header"
-          defaultMessage="SnapIT Hacktoberfest Starts In:"
-        />
-      </h1>
-      <CountdownTimer data-test="countdown-display" />
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+    >
+      {!isHacktoberfest ? (
+        <>
+          <h1 data-test="heading-display">
+            <FormattedMessage
+              id="Home-header"
+              defaultMessage="SnapIT Hacktoberfest Starts In:"
+            />
+          </h1>
+          <CountdownTimer data-test="countdown-display" />
+        </>
+      ) : (
+        <>
+          <h1 data-test="heading-display">
+            <FormattedMessage
+              id="Home-begun"
+              defaultMessage="HacktoberFest is here!"
+            />
+          </h1>
+          <Link
+            href={HACKTOBERFEST_URL}
+            className="link"
+            target="_blank"
+            rel="noopener"
+            data-test="hf-link"
+          >
+            <FormattedMessage
+              id="Home-link"
+              defaultMessage="Sign up to start hacking!"
+            />
+          </Link>
+        </>
+      )}
     </Grid>
   </div>
 );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 const DEFAULT_LANGUAGE = "en-US";
+export const HACKTOBERFEST_URL = "https://hacktoberfest.digitalocean.com/";
 
 export default DEFAULT_LANGUAGE;
