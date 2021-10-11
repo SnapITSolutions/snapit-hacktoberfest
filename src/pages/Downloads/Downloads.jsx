0edf530505cfda9e4afdf8ac97d4b6918695b89d
Steve Simmons <steve@snapit.solutions>
2021-10-11 14:16:29 -0500

    setup downloads page

---
+++ src/pages/Downloads/Downloads.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+
+const Downloads = () => (
+  <>
+    <Typography variant="h4">Downloads</Typography>
+  </>
+);
+
+export default Downloads;
