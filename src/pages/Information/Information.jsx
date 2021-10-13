64e738f639fcc66df4dc946edaaaa9bb8414ce5a
afif <afif@garasilabs.com>
2021-10-14 00:49:56 +0700

    Fix: Information page needs MUI

--- src/pages/Information/Information.jsx
+++ src/pages/Information/Information.jsx
@@ -9,22 +9,22 @@ const Information = () => (
     <div data-test="information-display">
       {informationData.map(({ header, paragraph }) => {
         const result = (
-          <div key={header.id}>
-            <Typography
-              m={4}
-              variant="h4"
-              data-test="heading-display"
-              textAlign="center"
-            >
+          <Box
+            key={header.id}
+            m={4}
+            textAlign="center"
+            data-test="information-item-display"
+          >
+            <Typography variant="h4" data-test="heading-display">
               <FormattedMessage id={header.id} defaultMessage={header.values} />
-              <Typography variant="body1" pt={1} data-test="para-display">
-                <FormattedMessage
-                  id={paragraph.id}
-                  defaultMessage={paragraph.values}
-                />
-              </Typography>
             </Typography>
-          </div>
+            <Typography variant="body1" pt={1} data-test="para-display">
+              <FormattedMessage
+                id={paragraph.id}
+                defaultMessage={paragraph.values}
+              />
+            </Typography>
+          </Box>
         );
         return result;
       })}
