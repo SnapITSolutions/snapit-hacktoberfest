5848694fff33ad0ef56e591cbea55aff8a36524c
ssimmons05 <73480835+ssimmons05@users.noreply.github.com>
2021-10-13 08:54:31 -0500

    refactor navigation list to an array (#116)

---
+++ src/components/Navigation/navigationData.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Home,
+  Login,
+  Help,
+  ImportContacts,
+  Person,
+  AddToQueue,
+  Assignment,
+  Download,
+} from "@mui/icons-material";
+
+const navigationData = [
+  {
+    icon: <Home />,
+    name: { id: "Home-nav", endpoint: "/", defaultMessage: "Home" },
+  },
+  {
+    icon: <ImportContacts />,
+    name: {
+      id: "Information-nav",
+      endpoint: "/information",
+      defaultMessage: "Information",
+    },
+  },
+  {
+    icon: <Help />,
+    name: { id: "FAQ-nav", endpoint: "/faq", defaultMessage: "FAQ" },
+  },
+  {
+    icon: <Login />,
+    name: { id: "Login-nav", endpoint: "/login", defaultMessage: "Login" },
+  },
+  {
+    icon: <Person />,
+    name: {
+      id: "Profile-nav",
+      endpoint: "/profile",
+      defaultMessage: "Profile",
+    },
+  },
+  {
+    icon: <AddToQueue />,
+    name: {
+      id: "Contributor-nav",
+      endpoint: "/contributor",
+      defaultMessage: "Contributor",
+    },
+  },
+  {
+    icon: <Assignment />,
+    name: {
+      id: "Registration-nav",
+      endpoint: "/registration",
+      defaultMessage: "Registration",
+    },
+  },
+  {
+    icon: <Download />,
+    name: {
+      id: "Downloads-nav",
+      endpoint: "/downloads",
+      defaultMessage: "Downloads",
+    },
+  },
+];
+
+export default navigationData;
