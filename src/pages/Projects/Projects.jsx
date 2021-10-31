99cb0b1043e5843ed44e4e7ac3d4d8828db8a495
Ismail <80280451+Plvtinum-1@users.noreply.github.com>
2021-10-31 03:29:23 +0100

    Created the Projects page (#120)
    * added the projects page
    
    * added the projects page
    
    * added vite and changed some css
    
    * replaced axios with fetch
    
    * fixed margin issue and buttons
    
    * removed debug log

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import {
+  Grid,
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+} from "@mui/material";
+
+const Projects = () => {
+  const [fetchedData, setfetchedData] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.github.com/search/repositories?q=stars:>100000&per_page=6"
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => setfetchedData(data))
+      .catch((error) => console.error("Error fetching data", error));
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h2" gutterBottom>
+        Projects Section
+      </Typography>
+      {fetchedData.length === 0 ? (
+        <Grid className="grid" container>
+          <CardActions>
+            <Typography variant="h4">Loading ...</Typography>
+          </CardActions>
+        </Grid>
+      ) : (
+        <Grid className="grid" container spacing={10}>
+          {fetchedData.items.map((item) => (
+            <Card className="card" sx={{ maxWidth: 440 }} key={item.id}>
+              <CardMedia
+                component="img"
+                height="240"
+                image={item.owner.avatar_url}
+                alt={item.name}
+              />
+              <CardActions>
+                <div className="body">
+                  <Typography variant="h4" gutterBottom>
+                    {item.name}
+                  </Typography>
+                  <Typography variant="body2">{item.description}</Typography>
+                  <Button
+                    className="button"
+                    variant="contained"
+                    type="button"
+                    size="small"
+                    href={item.homepage}
+                  >
+                    Homepage
+                  </Button>
+                  <Button
+                    className="button"
+                    variant="contained"
+                    type="button"
+                    size="small"
+                    href={item.html_url}
+                  >
+                    Github
+                  </Button>
+                </div>
+              </CardActions>
+            </Card>
+          ))}
+        </Grid>
+      )}
+    </>
+  );
+};
+
+export default Projects;
